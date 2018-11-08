ec1832d8f4995a32b9aff11d7ce2ebfda4e0cd02
Daniel Schädler <dschaedler@united-internet.de>
2018-11-08 10:35:51 +0100

    Tests for Group Stage class

--- spec/group_stage_spec.rb
+++ spec/group_stage_spec.rb
@@ -1,7 +1,16 @@
-require '../lib/turniere/group_stage'
-
-RSpec.describe Turniere do
-  it "calculates the right matchnumber" do
-    expect(Turniere::GroupStage.calculate_required_stage_count(23)).to eq(4)
+RSpec.describe Turniere::GroupStage do
+  [
+    { test: 23, result: 4 },
+    { test: 4, result: 2 },
+    { test: 378, result: 8 },
+    { test: 64, result: 6 },
+    { test: 2, result: 1 },
+    { test: 4711, result: 12 },
+    { test: 815, result: 9 },
+    { test: 12, result: 3 }
+  ].each do |parameters|
+    it "calculates #{parameters[:result]} playoff stages for #{parameters[:test]} teams" do
+      expect(Turniere::GroupStage.calculate_required_stage_count(parameters[:test])).to eq(parameters[:result])
+    end
   end
 end
